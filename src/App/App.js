bc412e7edcfcd280019a6366088290a0a0eb9c90
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-11 20:33:23 -0400

    pushing Navbar

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import "../App/App.css";
+import Home from "./Home/Home";
+import Signup from "../SignUp/SignUp";
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
